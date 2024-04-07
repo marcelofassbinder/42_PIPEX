6c507c9d23fab5cb2345d0a51b46ba7ce4287a6b
Marcelo Fassbinder Martins <mfassbin@c2r1s6.42porto.com>
2024-04-07 13:49:35 +0100

    utils_bonus added

--- bonus/pipex_bonus.h
+++ bonus/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 13:18:57 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/04/06 18:05:39 by mfassbin         ###   ########.fr       */
+/*   Updated: 2024/04/07 13:48:50 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ typedef struct  s_pipex_b
     int         processes;
     int         pid;
     int         **pipes;
+    int         **commands;
+    char        **path;
 }               t_pipex_b;
+
+char    *define_command(t_pipex_b *ppx, char *cmd);
 
 #endif
--- bonus/utils_bonus.c
+++ bonus/utils_bonus.c
@@ -6,9 +6,25 @@
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 15:25:52 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/04/06 15:31:33 by mfassbin         ###   ########.fr       */
+/*   Updated: 2024/04/07 13:48:48 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
+//funcao que retorna o comando, usada na exec_command
+char    *define_command(t_pipex_b *ppx, char *cmd)
+{
+    char    *command;
+
+    if (ft_strchr(cmd, 39) != NULL)
+        command = ft_split_trim(cmd, 39);
+    else
+        command = ft_split(cmd, ' ');
+    
+}
+/* 
+if (ft_strchr(argv[2], 39) != NULL)
+		ppx.cmd1 = ft_split_trim(argv[2], 39);
+	else
+		ppx.cmd1 = ft_split(argv[2], ' '); */
