086aefe6de13079936754d03e07272af2abbcdb6
Marcelo Fassbinder Martins <mfassbin@c2r1s7.42porto.com>
2024-04-08 17:26:32 +0100

    separating processes

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/06 13:18:30 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/04/08 12:32:21 by mfassbin         ###   ########.fr       */
+/*   Created: 2024/04/08 13:15:11 by mfassbin          #+#    #+#             */
+/*   Updated: 2024/04/08 17:25:26 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int here_doc(char **argv)
 {
     int     fd;
     char    *line;
+	char	*limit;
 
+	limit = argv[2];
     fd = open("here_doc", O_CREAT | O_WRONLY | O_TRUNC, 0666);
     if (fd < 0)
     {
@@ -27,8 +29,8 @@ int here_doc(char **argv)
     {
         ft_printf(1, ">");
         line = get_next_line(0);
-        if (ft_strncmp(line, argv[2], ft_strlen(argv[2])) == 0 && 
-            ft_strlen(line) == ft_strlen(argv[2]) + 1)
+        if (ft_strncmp(line, limit, ft_strlen(limit)) == 0 && 
+            ft_strlen(line) == ft_strlen(limit) + 1)
         {
             free(line);
             break;
@@ -59,116 +61,88 @@ void	exec_command(t_pipex_b *ppx, int i, char **argv, char **envp)
             command = ft_split(argv[2 + i], ' ');
 	}
 	j = -1;
-	while(ppx->path[j++])
+	while(ppx->path[++j])
 	{
-		ppx->path[j] = ft_strjoin(ppx->path[j], command[i]);
-		execve(ppx->path[j], &command[i], envp);
+		ppx->path[j] = ft_strjoin(ppx->path[j], command[0]);
+		execve(ppx->path[j], command, envp);
 	}
 	perror("execve");
 }
 
-void	first_process(t_pipex_b *ppx, char **argv, int i, char **envp)
+int open_infile(t_pipex_b *ppx, char **argv)
 {
 	int infile;
 
+	if (ppx->here_doc == 0)
+	{
+		infile = open(argv[1], O_RDONLY);
+		if (infile < 0)
+			perror("open infile");
+	}
 	if (ppx->here_doc == 1)
 		infile = here_doc(argv);
-	else
-		infile = open(argv[1],  O_WRONLY, 0666);
-	if (infile < 0)
-		perror(argv[1]);
-	if (pipe(ppx->pipes[0]) == -1)
-		perror("pipe");
-	close(ppx->pipes[0][0]);
-	dup2(infile, STDIN_FILENO);
-	dup2(ppx->pipes[0][1], STDOUT_FILENO);
-	close(ppx->pipes[0][1]);
-	close(infile);
+	return (infile);
+}
+
+void	last_process(t_pipex_b *ppx, int i, int argc, char **argv, char **envp)
+{
+	int	outfile;
+
+	outfile = open(argv[argc - 1], O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	if (outfile == -1)
+		perror("open outfile");
+	dup2(outfile, STDOUT_FILENO);
+	close(ppx->fd[1]);
+	close(outfile);
+	exec_command(ppx, i, argv, envp);
+	perror("execve");
+}
+
+void	child_process_b(t_pipex_b *ppx, int fd_redir, int i, char **argv, char **envp)
+{
+	dup2(fd_redir, STDIN_FILENO); //transforma o fd do infile ou do pipe anterior no input deste processo
+	close(fd_redir);//fecha o fd do infile ou do pipe anterior
+	dup2(ppx->fd[1], STDOUT_FILENO);//transforma o fd de escrita do pipe no output do processo
+	close(ppx->fd[0]);// fecha o fd de leitura
+	close(ppx->fd[1]);// fecha o fd de escrita
 	if (ppx->here_doc == 1)
-	{
-    	if (unlink("here_doc") < 0)
-    	{
-        	perror("unlink");
-        	exit(EXIT_FAILURE);
-    	}
-	}
-	exec_command(ppx, i, argv, envp);
+		unlink("here_doc");//deleta o arquivo "here_doc"
+	exec_command(ppx, i, argv, envp);//executa o comando
 }
 
-void	mid_process(t_pipex_b *ppx, int i, char **argv, char **envp)
+void	parent_process_b(t_pipex_b *ppx, int fd_redir)
 {
-	if(pipe(ppx->pipes[i]) == -1)
-		perror("pipe");
-	close(ppx->pipes[i][1]);
-	dup2(ppx->pipes[i -1][1], STDIN_FILENO);
-	dup2(ppx->pipes[i][0], STDOUT_FILENO);
-	close(ppx->pipes[i][0]);
-	close(ppx->pipes[i - 1][1]);
-	exec_command(ppx, i, argv, envp);
-}
-
-void	last_process(t_pipex_b *ppx, char **argv, int argc, int i, char **envp)
-{
-	int outfile;
-
-	outfile = open(argv[argc -1], O_WRONLY | O_TRUNC | O_CREAT, 0666);
-	if (outfile < 0)
-		perror(argv[argc - 1]);
-	if (pipe(ppx->pipes[i]) == -1)
-		perror("pipe");
-	close(ppx->pipes[i][1]);
-	dup2(ppx->pipes[i - 1][1], STDIN_FILENO);
-	dup2(ppx->pipes[i][0], STDOUT_FILENO);
-	close(ppx->pipes[i][0]);
-	close(ppx->pipes[i - 1][1]);
-	exec_command(ppx, i, argv, envp);
-}
-
-t_pipex_b init_bonus(int argc, char **argv, char **envp)
-{
-	t_pipex_b	ppx;
-
-	ppx.here_doc = 0;
-	ppx.processes = argc - 3;
-	if (ft_strcmp(argv[1], "here_doc") == 0)
-	{
-		ppx.here_doc = 1;
-		ppx.processes = argc - 4;
-	}
-	ppx.pipes = ft_calloc(sizeof(int *), ppx.processes - 1);
-	ppx.path = ft_path(envp);
-	ppx.pid = 0;
-	return(ppx);
+	//wait(NULL);
+	close(fd_redir);
+	close(ppx->fd[1]);
+	fd_redir = ppx->fd[0];
 }
 
 int main(int argc, char **argv, char **envp)
 {
 	t_pipex_b   ppx;
+	int			fd_redir;
 	int			i;
 
+	handle_input_b(argc, argv);
     ppx = init_bonus(argc, argv, envp);
+	fd_redir = open_infile(&ppx, argv);
 	i = 0;
-	while(i < ppx.processes)
+	while(i < ppx.processes - 1)
 	{
+		if (pipe(ppx.fd) == -1)
+			perror("pipe");
 		ppx.pid = fork();
+		if (ppx.pid == -1)
+			perror("fork");
 		if (ppx.pid == 0)
-		{
-			if (i == 0)
-				first_process(&ppx, argv, i, envp);
-			else
-				mid_process(&ppx, i, argv, envp);
-		}
+			child_process_b(&ppx, fd_redir, i, argv, envp);
 		if (ppx.pid > 0)
-			return (0);
+			parent_process_b(&ppx, fd_redir);
 		i++;
 	}
-	last_process(&ppx, argv, argc, i, envp);
-	/* ppx = init_struct(argv, envp);
-	if (pipe(ppx.fd) == -1)
-		free_and_exit(&ppx, "pipe", EXIT_FAILURE);
-	ppx.pid = fork();
-	if (ppx.pid == 0) // PROCESSO FILHO
-		child_process(&ppx, argv[1], envp);
-	if (ppx.pid > 0) // PROCESSO PAI
-		parent_process(&ppx, argv[4], envp); */
+	dup2(fd_redir, STDIN_FILENO);
+	close(fd_redir);
+	last_process(&ppx, i, argc, argv, envp);
 }
+
--- bonus/utils_bonus.c
+++ bonus/utils_bonus.c
@@ -6,47 +6,43 @@
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 15:25:52 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/04/07 17:49:50 by mfassbin         ###   ########.fr       */
+/*   Updated: 2024/04/08 16:57:02 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-/* char    **define_command(int argc, char **argv, int here_doc)
+t_pipex_b init_bonus(int argc, char **argv, char **envp)
 {
-    char    **command;
-    int     i;
+	t_pipex_b	ppx;
 
-    if (here_doc == 1)
-    {
-        command = ft_calloc(sizeof(char *), argc - 3);
-        i = 0;
-        while(i < argc - 3)
-        {
-            if (ft_strchr(argv[3 + i], 39) != NULL)
-                command = ft_split_trim(argv[3 + i], 39);
-            else
-                command = ft_split(argv[3 + i], ' ');
-            i++;
-        }
-    }
-    if (here_doc == 0)
-    {
-        command = ft_calloc(sizeof(char *), argc - 4);
-        i = 0;
-        while(i < argc - 4)
-        {
-            if (ft_strchr(argv[2 + i], 39) != NULL)
-                command[i] = ft_split_trim(argv[2 + i], 39);
-            else
-                command[i] = ft_split(argv[2 + i], ' ');
-            i++;
-        }
-    }
-    return (command);
-} */
-/* 
-if (ft_strchr(argv[2], 39) != NULL)
-		ppx.cmd1 = ft_split_trim(argv[2], 39);
-	else
-		ppx.cmd1 = ft_split(argv[2], ' '); */
+	ppx.here_doc = 0;
+	if (ft_strcmp(argv[1], "here_doc") == 0)
+		ppx.here_doc = 1;
+	ppx.processes = argc - 3 - ppx.here_doc;
+	ppx.fd[0] = 0;
+	ppx.fd[1] = 0;
+	ppx.path = ft_path(envp);
+	ppx.pid = 0;
+	return(ppx);
+}
+
+void	handle_input_b(int argc, char **argv)
+{
+	int i;
+	
+	if (argc < 5)
+	{
+		ft_printf(2, "Error\nCorrect usage: %s file1 cmd1 cmd2 file2\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	i = -1;
+	while(argv[++i])
+	{
+		if(argv[i][0] == 0)
+		{
+			ft_printf(2, "Error\nInvalid arguments\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
