5d3599fc21d24883848639db5cdb46959ddd4dfe
Marcelo Fassbinder Martins <mfassbin@c2r1s7.42porto.com>
2024-04-08 12:36:48 +0100

    creating branch

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mfassbin <mfassbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 13:18:30 by mfassbin          #+#    #+#             */
-/*   Updated: 2024/04/07 18:36:48 by mfassbin         ###   ########.fr       */
+/*   Updated: 2024/04/08 12:32:21 by mfassbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,4 +139,36 @@ t_pipex_b init_bonus(int argc, char **argv, char **envp)
 	ppx.path = ft_path(envp);
 	ppx.pid = 0;
 	return(ppx);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	t_pipex_b   ppx;
+	int			i;
+
+    ppx = init_bonus(argc, argv, envp);
+	i = 0;
+	while(i < ppx.processes)
+	{
+		ppx.pid = fork();
+		if (ppx.pid == 0)
+		{
+			if (i == 0)
+				first_process(&ppx, argv, i, envp);
+			else
+				mid_process(&ppx, i, argv, envp);
+		}
+		if (ppx.pid > 0)
+			return (0);
+		i++;
+	}
+	last_process(&ppx, argv, argc, i, envp);
+	/* ppx = init_struct(argv, envp);
+	if (pipe(ppx.fd) == -1)
+		free_and_exit(&ppx, "pipe", EXIT_FAILURE);
+	ppx.pid = fork();
+	if (ppx.pid == 0) // PROCESSO FILHO
+		child_process(&ppx, argv[1], envp);
+	if (ppx.pid > 0) // PROCESSO PAI
+		parent_process(&ppx, argv[4], envp); */
 }
